294e624d8da00278e04c70b40907f860c8de543c
SnigdhAdh <111344743+SnigdhAdh@users.noreply.github.com>
2023-04-10 18:21:46 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+video="";
+status="";
+
+
+function preload(){
+    video=createVideo("video.mp4");
+    
+    
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video.hide();
+
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="status: objects detecting";
+}
+
+function modelLoaded(){
+    console.log("model loaded");
+    status=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+    
+}
